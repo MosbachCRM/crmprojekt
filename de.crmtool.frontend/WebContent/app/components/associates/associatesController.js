1961f1801c35b031bc9c5ff953ac2c5c1314e4ac
Michael <mike@emmvier.com>
2017-01-19 19:19:28 +0100

    added more

--- de.crmtool.frontend/WebContent/app/components/associates/associatesController.js
+++ de.crmtool.frontend/WebContent/app/components/associates/associatesController.js
@@ -2,7 +2,7 @@ var associatesController = angular.module('associatesController', []);
 
 associatesController.controller('AssociatesCtrl', [
 	'$scope', '$route', '$http', '$location', function($scope, $route, $http, $location){
-       	$http.get('http://r51z.ucc.ovgu.de:8000/zrest_service?sap-client=204')
+       	$http.get('http://r51z.ucc.ovgu.de/zrest_service?sap-client=204')
     	.then(function successCallback(response) {
     		$scope.associates = response.data;
     	});
